7dcdacbbe2ea14b8e336df9cdb6d074b56f0e04c
Josh Kuttler <yoshuakuttler@gmail.com>
2020-12-22 21:55:41 +0200

    add composition, test and docs

--- src/elements/button/button.spec.tsx
+++ src/elements/button/button.spec.tsx
@@ -2,60 +2,53 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 
-import { Button } from './button';
+import { PrimaryButton, SecondaryButton, NormalButton } from './button.composition';
 
 it('should render', () => {
-	const { getByText } = render(<Button>I am primary button</Button>);
-	const rendered = getByText('I am primary button');
+	const { getByText } = render(<PrimaryButton />);
+	const rendered = getByText('Primary');
+
+	expect(rendered).to.exist;
+});
+
+it('should render secondary', () => {
+	const { getByText } = render(<SecondaryButton />);
+	const rendered = getByText('Secondary');
 
 	expect(rendered).to.exist;
 });
 
 it('should pass classname', () => {
-	const { getByText } = render(
-		<Button className="test-class">I am primary button</Button>
-	);
-	const rendered = getByText('I am primary button');
+	const { getByText } = render(<PrimaryButton className="test-class" />);
+	const rendered = getByText('Primary');
 
 	expect(rendered.className).to.include('test-class');
 });
 
 it('should use variation html attribute', () => {
-	const { getByText } = render(
-		<Button className="test-class">I am primary button</Button>
-	);
-	const rendered = getByText('I am primary button');
+	const { getByText } = render(<NormalButton className="test-class" />);
+	const rendered = getByText('Normal');
 
-	expect(rendered.getAttribute('data-variation')).to.equal('normal')
+	expect(rendered.getAttribute('data-variation')).to.equal('normal');
 });
 
 it('should have "cta" as html attribute, when using cta variation', () => {
-	const { getByText } = render(
-		<Button importance="cta" className="test-class">I am primary button</Button>
-	);
-	const rendered = getByText('I am primary button');
+	const { getByText } = render(<PrimaryButton importance="cta" className="test-class" />);
+	const rendered = getByText('Primary');
 
-	expect(rendered.getAttribute('data-variation')).to.equal('cta')
-})
+	expect(rendered.getAttribute('data-variation')).to.equal('cta');
+});
 
-// css modules checks do not work with mocha tester, yet
+it('should use low elevation by default', () => {
+	const { getByText } = render(<PrimaryButton className="test-class" />);
+	const rendered = getByText('Primary');
 
-// it('should use low elevation by default', () => {
-// 	const { getByText } = render(
-// 		<Button className="test-class">I am primary button</Button>
-// 	);
-// 	const rendered = getByText('I am primary button');
+	expect(rendered.className).to.include('low');
+});
 
-// 	expect(rendered.className).to.include('low');
-// });
+it('should use specific elevation', () => {
+	const { getByText } = render(<PrimaryButton className="test-class" elevation="high" />);
+	const rendered = getByText('Primary');
 
-// it('should use specific elevation', () => {
-// 	const { getByText } = render(
-// 		<Button className="test-class" elevation="high">
-// 			I am primary button
-// 		</Button>
-// 	);
-// 	const rendered = getByText('I am primary button');
-
-// 	expect(rendered.className).to.include('hight');
-// });
+	expect(rendered.className).to.include('high');
+});
