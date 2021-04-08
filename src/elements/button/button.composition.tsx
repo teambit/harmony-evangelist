d692548c8f9e1d844dc17cef54a4f69dc063800a
Uri Kutner <urikutner@gmail.com>
2021-04-08 21:19:50 +0300

    refresh button docs

--- src/elements/button/button.composition.tsx
+++ src/elements/button/button.composition.tsx
@@ -2,73 +2,103 @@ import React from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { Button } from './button';
 
-export const PrimaryButton = ({ ...rest }) => {
+export const CallToActionButton = ({ ...rest }) => {
 	return (
 		<ThemeCompositions>
 			<Button importance="cta" style={{ width: 120 }} {...rest}>
-				Primary
+				Submit
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
-export const SecondaryButton = () => {
+export const MainButton = () => {
 	return (
 		<ThemeCompositions>
 			<Button importance="ghost" style={{ width: 120 }}>
-				Secondary
+				Update
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
-export const NormalButton = ({ ...rest }) => {
+export const LightButton = ({ ...rest }) => {
 	return (
 		<ThemeCompositions>
 			<Button style={{ width: 120 }} {...rest}>
-				Normal
+				learn more
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
-export const PrimaryLoadingButton = () => {
+export const CallToActionLoading = () => {
 	return (
 		<ThemeCompositions>
 			<Button importance="cta" loading style={{ width: 120 }}>
-				Primary
+				Submit
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
-export const SecondaryLoadingButton = () => {
+export const Loading = () => {
 	return (
 		<ThemeCompositions>
 			<Button importance="ghost" loading style={{ width: 120 }}>
-				Secondary
+				Update
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
-export const NormalLoadingButton = () => {
+export const LightButtonLoading = () => {
 	return (
 		<ThemeCompositions>
 			<Button loading style={{ width: 120 }}>
-				Normal
+				Learn more
+			</Button>
+		</ThemeCompositions>
+	);
+};
+
+export const CallToActionButtonDisabled = ({ ...rest }) => {
+	return (
+		<ThemeCompositions>
+			<Button importance="cta" style={{ width: 120 }} {...rest} disabled>
+				Submit
+			</Button>
+		</ThemeCompositions>
+	);
+};
+
+export const MainButtonDisabled = () => {
+	return (
+		<ThemeCompositions>
+			<Button importance="ghost" style={{ width: 120 }} disabled>
+				Update
+			</Button>
+		</ThemeCompositions>
+	);
+};
+
+export const LightButtonDisabled = ({ ...rest }) => {
+	return (
+		<ThemeCompositions>
+			<Button style={{ width: 120 }} {...rest} disabled>
+				learn more
 			</Button>
 		</ThemeCompositions>
 	);
 };
 
 const compositions = [
-	PrimaryButton,
-	SecondaryButton,
-	NormalButton,
-	PrimaryLoadingButton,
-	SecondaryLoadingButton,
-	NormalLoadingButton,
+	CallToActionButton,
+	MainButton,
+	LightButton,
+	CallToActionLoading,
+	Loading,
+	LightButtonLoading,
 ];
 // @ts-ignore
 compositions.map((comp) => (comp.canvas = { height: 90 }));
--- src/elements/button/button.spec.tsx
+++ src/elements/button/button.spec.tsx
@@ -2,52 +2,52 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 
-import { PrimaryButton, SecondaryButton, NormalButton } from './button.composition';
+import { CallToActionButton, MainButton, LightButton } from './button.composition';
 
 it('should render', () => {
-	const { getByText } = render(<PrimaryButton />);
+	const { getByText } = render(<CallToActionButton />);
 	const rendered = getByText('Primary');
 
 	expect(rendered).to.exist;
 });
 
 it('should render secondary', () => {
-	const { getByText } = render(<SecondaryButton />);
+	const { getByText } = render(<MainButton />);
 	const rendered = getByText('Secondary');
 
 	expect(rendered).to.exist;
 });
 
 it('should pass classname', () => {
-	const { getByText } = render(<PrimaryButton className="test-class" />);
+	const { getByText } = render(<CallToActionButton className="test-class" />);
 	const rendered = getByText('Primary');
 
 	expect(rendered.className).to.include('test-class');
 });
 
 it('should use variation html attribute', () => {
-	const { getByText } = render(<NormalButton className="test-class" />);
+	const { getByText } = render(<LightButton className="test-class" />);
 	const rendered = getByText('Normal');
 
 	expect(rendered.getAttribute('data-variation')).to.equal('normal');
 });
 
 it('should have "cta" as html attribute, when using cta variation', () => {
-	const { getByText } = render(<PrimaryButton importance="cta" className="test-class" />);
+	const { getByText } = render(<CallToActionButton importance="cta" className="test-class" />);
 	const rendered = getByText('Primary');
 
 	expect(rendered.getAttribute('data-variation')).to.equal('cta');
 });
 
 it('should use low elevation by default', () => {
-	const { getByText } = render(<PrimaryButton className="test-class" />);
+	const { getByText } = render(<CallToActionButton className="test-class" />);
 	const rendered = getByText('Primary');
 
 	expect(rendered.className).to.include('low');
 });
 
 it('should use specific elevation', () => {
-	const { getByText } = render(<PrimaryButton className="test-class" elevation="high" />);
+	const { getByText } = render(<CallToActionButton className="test-class" elevation="high" />);
 	const rendered = getByText('Primary');
 
 	expect(rendered.className).to.include('high');
