5ca9109d0a3e9a9a0f91d2f167d2eb05bd15bdfc
Josh Kuttler <yoshuakuttler@gmail.com>
2021-04-18 17:56:56 +0300

    update enterprise section spec

--- src/sections/hybrid-homepage/enterprise-section/enterprise-section.spec.tsx
+++ src/sections/hybrid-homepage/enterprise-section/enterprise-section.spec.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-//import { expect } from 'chai';
-
 import { EnterpriseSectionExample } from './enterprise-section.composition';
 
 it('should render correctly', () => {
