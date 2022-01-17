5eb59a2210f5f9c0b1107049162f57751027a812
Josh Kuttler <yoshuakuttler@gmail.com>
2022-01-17 19:20:59 +0200

    update checkbox to pass classname prop

--- src/input/checkbox/label/checkbox-label.tsx
+++ src/input/checkbox/label/checkbox-label.tsx
@@ -1,26 +1,22 @@
 import React, { isValidElement } from 'react';
-import styles from './checkbox-label.module.scss';
+import classNames from 'classnames';
 import { CheckboxIndicator } from '@teambit/evangelist.input.checkbox.indicator';
 import {
 	CheckboxLabel as CheckboxLabelBase,
 	CheckboxLabelProps as BaseProps,
 } from '@teambit/base-ui.input.checkbox.label';
+import styles from './checkbox-label.module.scss';
 
 export type CheckboxLabelProps = BaseProps;
 
 /** A styled checkbox with text. */
-export function CheckboxLabel(props: BaseProps) {
-	const child =
-		isValidElement(props.children) || !!props.children ? (
-			props.children
-		) : (
-			<span>{props.children}</span>
-		);
+export function CheckboxLabel({ children, className, ...rest }: BaseProps) {
+	const child = isValidElement(children) || !!children ? children : <span>{children}</span>;
 
 	return (
 		<CheckboxLabelBase
-			{...props}
-			className={styles.checkboxLabel}
+			{...rest}
+			className={classNames(styles.checkboxLabel, className)}
 			indicator={<CheckboxIndicator />}
 		>
 			{child}
