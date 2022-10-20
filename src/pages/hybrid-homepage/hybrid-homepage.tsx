99206402a1cc56ec473f9677ae0570fb10adad56
Josh Kuttler <35892475+JoshK2@users.noreply.github.com>
2022-10-20 23:17:14 +0300

    update book a demo buttons in home and enterprise pages (#40)

--- src/pages/hybrid-homepage/hybrid-homepage.tsx
+++ src/pages/hybrid-homepage/hybrid-homepage.tsx
@@ -1,11 +1,7 @@
 import React, { PureComponent, HTMLAttributes, ReactNode } from 'react';
 import classNames from 'classnames';
-
-import styles from './hybrid-homepage.module.scss';
-
 import { backgrounds } from '@teambit/base-ui.surfaces.background';
 import { centerColumn, wideColumn } from '@teambit/base-ui.layout.page-frame';
-
 import { CarouselSection } from '@teambit/evangelist.sections.carousel-section';
 import { CtaSection } from '@teambit/evangelist.sections.cta';
 import { EnterpriseSection } from '@teambit/evangelist.sections.enterprise-section';
@@ -19,16 +15,17 @@ import { ReuseComponents } from '@teambit/evangelist.sections.reuse-components';
 import { BuildTogether } from '@teambit/evangelist.sections.build-together';
 import { DiscoverComponents } from '@teambit/evangelist.sections.discover-components';
 import { EnterpriseLogos } from '@teambit/evangelist.marketing.enterprise-logos';
+import styles from './hybrid-homepage.module.scss';
 
 export type HybridHomepageProps = {
-	/** placeholder for content, after the first purple background fold. */
-	communityScopes?: ReactNode;
-	/** placeholder for content, at the bottom of the first purple background fold */
-	mainCta?: ReactNode;
-	/** show github star count at the community section */
-	githubStars?: number;
-	/** handles 'book meeting' call to action */
-	onBookMeeting?: () => any | Promise<any>;
+  /** placeholder for content, after the first purple background fold. */
+  communityScopes?: ReactNode;
+  /** placeholder for content, at the bottom of the first purple background fold */
+  mainCta?: ReactNode;
+  /** show github star count at the community section */
+  githubStars?: number;
+  /** handles 'book meeting' call to action */
+  onBookMeeting?: () => any | Promise<any>;
 } & HTMLAttributes<HTMLDivElement>;
 
 /**
@@ -39,90 +36,51 @@ export type HybridHomepageProps = {
  * @name HybridHomepage
  */
 export class HybridHomepage extends PureComponent<HybridHomepageProps> {
-	render() {
-		const {
-			communityScopes = null,
-			mainCta,
-			githubStars,
-			onBookMeeting,
-			className,
-			...rest
-		} = this.props;
+  render() {
+    const { communityScopes = null, mainCta, githubStars, onBookMeeting, className, ...rest } = this.props;
 
-		return (
-			<div {...rest} className={classNames(backgrounds.bedrock, className)}>
-				<div
-					className={classNames(
-						styles.purpleBackground,
-						styles.purpleFold,
-						styles.margin180
-					)}
-				>
-					<Hero className={classNames(styles.landingSection)} />
+    return (
+      <div {...rest} className={classNames(backgrounds.bedrock, className)}>
+        <div className={classNames(styles.purpleBackground, styles.purpleFold, styles.margin180)}>
+          <Hero className={classNames(styles.landingSection)} onBookMeeting={onBookMeeting} />
 
-					{mainCta}
-				</div>
+          {mainCta}
+        </div>
 
-				{communityScopes}
+        {communityScopes}
 
-				<EnterpriseSection className={classNames(centerColumn, styles.margin100)} />
+        <EnterpriseSection className={classNames(centerColumn, styles.margin100)} />
 
-				<EnterpriseLogos className={styles.margin130} />
+        <EnterpriseLogos className={styles.margin130} />
 
-				<CarouselSection className={classNames(wideColumn)} />
+        <CarouselSection className={classNames(wideColumn)} />
 
-				<div className={styles.uEllipse}></div>
+        <div className={styles.uEllipse}></div>
 
-				<div
-					className={classNames(
-						styles.greyBackground,
-						styles.greyBgPadding,
-						styles.foldMargin
-					)}
-				>
-					<DevelopComponents
-						className={classNames(centerColumn, styles.foldMargin)}
-					/>
+        <div className={classNames(styles.greyBackground, styles.greyBgPadding, styles.foldMargin)}>
+          <DevelopComponents className={classNames(centerColumn, styles.foldMargin)} />
 
-					<ComponentsAdvantages className={classNames(centerColumn)} />
-				</div>
+          <ComponentsAdvantages className={classNames(centerColumn)} />
+        </div>
 
-				<ReuseComponents className={classNames(centerColumn, styles.foldMargin)} />
+        <ReuseComponents className={classNames(centerColumn, styles.foldMargin)} />
 
-				<TalkToExperts
-					className={classNames(centerColumn, styles.foldMargin)}
-					onBookMeeting={onBookMeeting}
-				/>
+        <TalkToExperts className={classNames(centerColumn, styles.foldMargin)} onBookMeeting={onBookMeeting} />
 
-				<div
-					className={classNames(
-						styles.greyBackground,
-						styles.greyBgPadding,
-						styles.foldMargin
-					)}
-				>
-					<BuildTogether className={classNames(centerColumn)} />
-				</div>
+        <div className={classNames(styles.greyBackground, styles.greyBgPadding, styles.foldMargin)}>
+          <BuildTogether className={classNames(centerColumn)} />
+        </div>
 
-				<DiscoverComponents className={classNames(centerColumn, styles.foldMargin)} />
+        <DiscoverComponents className={classNames(centerColumn, styles.foldMargin)} />
 
-				<div
-					className={classNames(
-						styles.greyBackground,
-						styles.greyBgPadding,
-						styles.foldMargin
-					)}
-				>
-					<IndependentTeams className={classNames(centerColumn)} />
-				</div>
+        <div className={classNames(styles.greyBackground, styles.greyBgPadding, styles.foldMargin)}>
+          <IndependentTeams className={classNames(centerColumn)} />
+        </div>
 
-				<CommunitySection
-					className={classNames(centerColumn, styles.foldMargin)}
-					githubStars={githubStars}
-				/>
+        <CommunitySection className={classNames(centerColumn, styles.foldMargin)} githubStars={githubStars} />
 
-				<CtaSection className={classNames(centerColumn, styles.foldMargin)} />
-			</div>
-		);
-	}
+        <CtaSection className={classNames(centerColumn, styles.foldMargin)} />
+      </div>
+    );
+  }
 }
